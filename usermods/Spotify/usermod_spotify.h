cbf92f9582832f336b71937f765de18b5835b92d
Will Tatam <will@netmindz.net>
2024-09-08 12:34:00 +0100

    Spotify UserMod - fetching data

--- usermods/Spotify/usermod_spotify.h
+++ usermods/Spotify/usermod_spotify.h
@@ -20,87 +20,87 @@ void printCurrentlyPlayingToSerial(CurrentlyPlaying currentlyPlaying)
     // make sure you copy them (using something like strncpy)
     // const char* artist =
 
-    Serial.println("--------- Currently Playing ---------");
+    USER_PRINTLN("--------- Currently Playing ---------");
 
-    Serial.print("Is Playing: ");
+    USER_PRINT("Is Playing: ");
     if (currentlyPlaying.isPlaying)
     {
-        Serial.println("Yes");
+        USER_PRINTLN("Yes");
     }
     else
     {
-        Serial.println("No");
+        USER_PRINTLN("No");
     }
 
-    Serial.print("Track: ");
-    Serial.println(currentlyPlaying.trackName);
-    Serial.print("Track URI: ");
-    Serial.println(currentlyPlaying.trackUri);
-    Serial.println();
+    USER_PRINT("Track: ");
+    USER_PRINTLN(currentlyPlaying.trackName);
+    USER_PRINT("Track URI: ");
+    USER_PRINTLN(currentlyPlaying.trackUri);
+    USER_PRINTLN();
 
-    Serial.println("Artists: ");
+    USER_PRINTLN("Artists: ");
     for (int i = 0; i < currentlyPlaying.numArtists; i++)
     {
-        Serial.print("Name: ");
-        Serial.println(currentlyPlaying.artists[i].artistName);
-        Serial.print("Artist URI: ");
-        Serial.println(currentlyPlaying.artists[i].artistUri);
-        Serial.println();
+        USER_PRINT("Name: ");
+        USER_PRINTLN(currentlyPlaying.artists[i].artistName);
+        USER_PRINT("Artist URI: ");
+        USER_PRINTLN(currentlyPlaying.artists[i].artistUri);
+        USER_PRINTLN();
     }
 
-    Serial.print("Album: ");
-    Serial.println(currentlyPlaying.albumName);
-    Serial.print("Album URI: ");
-    Serial.println(currentlyPlaying.albumUri);
-    Serial.println();
+    USER_PRINT("Album: ");
+    USER_PRINTLN(currentlyPlaying.albumName);
+    USER_PRINT("Album URI: ");
+    USER_PRINTLN(currentlyPlaying.albumUri);
+    USER_PRINTLN();
 
     if (currentlyPlaying.contextUri != NULL)
     {
-        Serial.print("Context URI: ");
-        Serial.println(currentlyPlaying.contextUri);
-        Serial.println();
+        USER_PRINT("Context URI: ");
+        USER_PRINTLN(currentlyPlaying.contextUri);
+        USER_PRINTLN();
     }
 
     long progress = currentlyPlaying.progressMs; // duration passed in the song
     long duration = currentlyPlaying.durationMs; // Length of Song
-    Serial.print("Elapsed time of song (ms): ");
-    Serial.print(progress);
-    Serial.print(" of ");
-    Serial.println(duration);
-    Serial.println();
+    USER_PRINT("Elapsed time of song (ms): ");
+    USER_PRINT(progress);
+    USER_PRINT(" of ");
+    USER_PRINTLN(duration);
+    USER_PRINTLN();
 
     float percentage = ((float)progress / (float)duration) * 100;
     int clampedPercentage = (int)percentage;
-    Serial.print("<");
+    USER_PRINT("<");
     for (int j = 0; j < 50; j++)
     {
         if (clampedPercentage >= (j * 2))
         {
-            Serial.print("=");
+            USER_PRINT("=");
         }
         else
         {
-            Serial.print("-");
+            USER_PRINT("-");
         }
     }
-    Serial.println(">");
-    Serial.println();
+    USER_PRINTLN(">");
+    USER_PRINTLN();
 
     // will be in order of widest to narrowest
     // currentlyPlaying.numImages is the number of images that
     // are stored
     for (int i = 0; i < currentlyPlaying.numImages; i++)
     {
-        Serial.println("------------------------");
-        Serial.print("Album Image: ");
-        Serial.println(currentlyPlaying.albumImages[i].url);
-        Serial.print("Dimensions: ");
-        Serial.print(currentlyPlaying.albumImages[i].width);
-        Serial.print(" x ");
-        Serial.print(currentlyPlaying.albumImages[i].height);
-        Serial.println();
+        USER_PRINTLN("------------------------");
+        USER_PRINT("Album Image: ");
+        USER_PRINTLN(currentlyPlaying.albumImages[i].url);
+        USER_PRINT("Dimensions: ");
+        USER_PRINT(currentlyPlaying.albumImages[i].width);
+        USER_PRINT(" x ");
+        USER_PRINT(currentlyPlaying.albumImages[i].height);
+        USER_PRINTLN();
     }
-    Serial.println("------------------------");
+    USER_PRINTLN("------------------------");
 }
 
 class SpotifyUsermod : public Usermod {
@@ -109,20 +109,10 @@ class SpotifyUsermod : public Usermod {
 
     // Private class members. You can declare variables and functions only accessible to your usermod here
 
-    // set your config variables to their boot default value (this can also be done in readFromConfig() or a constructor if you prefer)
-    bool testBool = false;
-    unsigned long testULong = 42424242;
-    float testFloat = 42.42;
-    String testString = "Forty-Two";
-
     String clientId = "";
     String clientSecret = "";
     String refreshToken = "";
 
-    // These config variables have defaults set inside readFromConfig()
-    int testInt;
-    long testLong;
-    int8_t testPins[2];
 
     // any private methods should go here (non-inline method should be defined out of class)
     void publishMqtt(const char* state, bool retain = false); // example for publishing MQTT message
@@ -132,7 +122,7 @@ class SpotifyUsermod : public Usermod {
 
 
     unsigned long delayBetweenRequests = 10000; // Time between requests (1 minute)
-    unsigned long requestDueTime;               //time when request due
+    unsigned long requestDueTime = 0;           //time when request due
 
 
   public:
@@ -148,6 +138,7 @@ class SpotifyUsermod : public Usermod {
      * You can use it to initialize variables, sensors or similar.
      */
     void setup() {
+      spotify.setRefreshToken(refreshToken.c_str());
       initDone = true;
     }
 
@@ -157,34 +148,29 @@ class SpotifyUsermod : public Usermod {
      * Use it to initialize network interfaces
      */
     void connected() {
-      Serial.println("Connected to WiFi!");
+      if (!enabled) return;
+      USER_PRINTLN("Connected to WiFi!");
       client.setCACert(spotify_server_cert);
       initDone = true;
       const char *refreshToken = NULL;
       String callbackURI = "http://" +  WiFi.localIP().toString() + "/settings/um?um=Spotify";
       if(false) {
       // if(strcmp(refreshToken.c_str(), "") == 0) {
-        Serial.println("Fetch refresh token");
-        refreshToken = spotify.requestAccessTokens("--", callbackURI.c_str());
+        USER_PRINTLN("Fetch refresh token");
+        refreshToken = spotify.requestAccessTokens("AQBhcQ4OCmawSPENF8ZWizBRjE9vawQvrOnpsZzzOy3NdsK37WrqyZSSBRK2toNCABH1AIPcXZSVK3H-ya77wS_bEcd85nX1XDYPFVKEEzIzy4klRqfg8RtCyR14a2xTY1ya7I_mmpJBvOb9ZEL0tfT97QNQu3_K8-XaBAPMxEfkAMyJYE_HTVn37I6Q5FIFDL5MmHHZvj3nV7C_u9aqd9dWASGLyv3tWhwi", callbackURI.c_str());
         spotify.setRefreshToken(refreshToken);
       }
 
-      Serial.println("Refreshing Access Tokens");
+      USER_PRINTLN("Refreshing Access Tokens");
       if (!spotify.refreshAccessToken())      {
           USER_PRINTLN("Failed to get access tokens");
       }
+      USER_PRINTLN("SpotifyUsermod::connected completed");
     }
 
 
     /*
      * loop() is called continuously. Here you can check for events, read sensors, etc.
-     * 
-     * Tips:
-     * 1. You can use "if (WLED_CONNECTED)" to check for a successful network connection.
-     *    Additionally, "if (WLED_MQTT_CONNECTED)" is available to check for a connection to an MQTT broker.
-     * 
-     * 2. Try to avoid using the delay() function. NEVER use delays longer than 10 milliseconds.
-     *    Instead, use a timer check as shown here.
      */
     void loop() {
       // if usermod is disabled or called during strip updating just exit
@@ -193,28 +179,25 @@ class SpotifyUsermod : public Usermod {
 
       // do your magic here
       if (millis() - lastTime > 1000) {
-        //Serial.println("I'm alive!");
+        // USER_PRINTLN("I'm alive!");
         lastTime = millis();
         if (millis() > requestDueTime)
         {
-          Serial.print("Free Heap: ");
-          Serial.println(ESP.getFreeHeap());
+          USER_PRINT("Free Heap: ");
+          USER_PRINTLN(ESP.getFreeHeap());
 
-          Serial.println("getting currently playing song:");
+          USER_PRINTLN("getting currently playing song:");
           // Market can be excluded if you want e.g. spotify.getCurrentlyPlaying()
           int status = spotify.getCurrentlyPlaying(printCurrentlyPlayingToSerial, "GB");
-          if (status == 200)
-          {
-              Serial.println("Successfully got currently playing");
+          if (status == 200){
+              USER_PRINTLN("Successfully got currently playing");
           }
-          else if (status == 204)
-          {
-              Serial.println("Doesn't seem to be anything playing");
+          else if (status == 204) {
+              USER_PRINTLN("Doesn't seem to be anything playing");
           }
-          else
-          {
-              Serial.print("Error: ");
-              Serial.println(status);
+          else {
+              USER_PRINT("Error: ");
+              USER_PRINTLN(status);
           }
           requestDueTime = millis() + delayBetweenRequests;
         }
@@ -374,16 +357,6 @@ class SpotifyUsermod : public Usermod {
       //publishMqtt("I am alive!");
     }
 #endif
-
-
-    /**
-     * onStateChanged() is used to detect WLED state change
-     * @mode parameter is CALL_MODE_... parameter used for notifications
-     */
-    void onStateChange(uint8_t mode) {
-      // do something if WLED state changed (color, brightness, effect, preset, etc)
-    }
-
 
     /*
      * getId() allows you to optionally give your V2 usermod an unique ID (please define it in const.h!).
