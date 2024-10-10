bba2154d4b8176df5bca8a93e935fd5a349e42a0
Will Tatam <will@netmindz.net>
2024-10-10 15:16:35 +0100

    Set WLED_VERSION

--- pio-scripts/set_version.py
+++ pio-scripts/set_version.py
@@ -1,8 +1,13 @@
 Import('env')
 import json
+import os
 
 PACKAGE_FILE = "package.json"
 
-with open(PACKAGE_FILE, "r") as package:
-    version = json.load(package)["version"]
-    env.Append(BUILD_FLAGS=[f"-DWLED_VERSION={version}"])
+if(os.environ.get("WLED_VERSION")):
+    version = os.environ.get("WLED_VERSION")
+else:
+    with open(PACKAGE_FILE, "r") as package:
+        version = json.load(package)["version"]
+
+env.Append(BUILD_FLAGS=[f"-DWLED_VERSION={version}"])
